cf91395cdf0a23549e1354b0e08797918076ab83
Tobias Davis <accounts@davistobias.com>
2015-05-08 22:09:00 -0500

    Add one more test to check subfolders.

--- test/test_files_in_folders.js
+++ test/test_files_in_folders.js
@@ -10,7 +10,20 @@ test('retrieve a post by filename', function(t) {
 
 	retrieve.getPost('folder1/folder-file-1.md', function(err, post) {
 		t.notOk(err)
-		t.equal(post.content, 'urp')
+		t.equal(post.content, 'one')
+		server.close()
+		t.end()
+	})
+})
+
+test('retrieve a post by filename', function(t) {
+	var server = fakeoServer(8989, '/content-with-directories')
+
+	var retrieve = new Retrieve('http://127.0.0.1:8989')
+
+	retrieve.getPost('folder1/folder2/folder-file-2.md', function(err, post) {
+		t.notOk(err)
+		t.equal(post.content, 'two')
 		server.close()
 		t.end()
 	})
